1136128b55041dd94eba424302f521c6bb7f9d61
Palmer Harrell <palmerharrell@me.com>
2016-06-15 20:37:21 -0500

    Adjusting ng-repeat and scope array to accept items from API instead of Firebase

--- app/factories/getFactory.js
+++ app/factories/getFactory.js
@@ -8,8 +8,9 @@ app.factory("getFactory", function($q, $http, apiURL, authFactory) {
       
       var currentUser = authFactory.getUser(); // get currently logged in user
 
-      // get only current user's items from firebase
-      $http.get(`${apiURL}/items/.json?orderBy="fbuid"&equalTo="${currentUser.uid}"`)
+      // get only current user's items from API
+      // Example URL: http://localhost:5000/api/mediaitem?userid=12
+      $http.get(`${apiURL}/mediaitem?userid=${currentUser.IdAppUser}`)
       .success(
         function(JSONobjFromGet) {
           resolve(JSONobjFromGet);
